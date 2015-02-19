789e6b0c2ecf0f43718fc1d748d38ca85480f209
Norbert Bartels <n.bartels@phpmonkeys.de>
2015-02-19 16:42:52 +0100

    First Benchmark added

---
+++ src/main/java/com/restfb/benchmark/SimpleDateFormatBenchmark.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2010-2015 Mark Allen.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.restfb.benchmark;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+@State(Scope.Thread)
+public class SimpleDateFormatBenchmark {
+
+    public static final String SHORT_DATE_FORMAT = "MM/dd/yyyy";
+
+    private final SimpleDateFormat myFormatter = new SimpleDateFormat(SHORT_DATE_FORMAT);
+    
+    @Benchmark
+    public void measureSimpleDateFormat() throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat(SHORT_DATE_FORMAT);
+	Date date = formatter.parse("09/22/2014");
+    }
+    
+    @Benchmark
+    public void measurePrivateSimpleDateFormat() throws ParseException {
+	Date date = myFormatter.parse("09/22/2014");
+    }
+
+    /*
+     * ============================== HOW TO RUN THIS TEST: ====================================
+     *
+     * You can run this test:
+     *
+     * a) Via the command line:
+     *    $ mvn clean install
+     *    $ java -ea -jar target/restfb-benchmarks.jar SimpleDateFormatBenchmark -wi 5 -i 5 -f 1
+     *    (we requested 5 warmup/measurement iterations, single fork)
+     *
+     * b) Via the Java API:
+     *    (see the JMH homepage for possible caveats when running from IDE:
+     *      http://openjdk.java.net/projects/code-tools/jmh/)
+     */
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(SimpleDateFormatBenchmark.class.getSimpleName())
+                .warmupIterations(5)
+                .measurementIterations(10)
+                .forks(1)
+                .jvmArgs("-ea")
+                .build();
+
+        new Runner(opt).run();
+    }
+}
