96c88fe338110b7c05aa252f3a39f83d78db5be7
Norbert Bartels <n.bartels@phpmonkeys.de>
2015-02-19 22:19:25 +0100

    JsonMapperBenchmark added (THX @profes)

---
+++ src/main/java/com/restfb/benchmark/JsonHelper.java
@@ -0,0 +1,15 @@
+package com.restfb.benchmark;
+
+import com.restfb.util.StringUtils;
+
+import java.io.IOException;
+
+public class JsonHelper {
+    static String jsonFromClasspath(String pathToJson) {
+        try {
+            return StringUtils.fromInputStream(JsonHelper.class.getResourceAsStream("/json/" + pathToJson + ".json"));
+        } catch (IOException e) {
+            throw new IllegalStateException("Unable to load JSON from the classpath", e);
+        }
+    }
+}
